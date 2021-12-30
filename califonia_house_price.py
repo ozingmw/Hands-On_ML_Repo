4336b452f7cf2eb10e552482531479992e815287
Jonghun Shin <69336570+ozingmw@users.noreply.github.com>
2021-12-30 18:54:20 +0900

    21.12.29

--- califonia_house_price.py
+++ califonia_house_price.py
@@ -1,5 +1,7 @@
-#Hand-On Machine Learning with Scikit-Learn, Keras & Tensorflow
-#21.12.28 ~
+# Hand-On Machine Learning with Scikit-Learn, Keras & Tensorflow
+# 21.12.28 ~
+# 엔터 두번 콜랩 1셀
+# VSC 전용 (colab하고 코드다름)
 
 import os
 import tarfile
@@ -17,10 +19,26 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
 
+
 import pandas as pd
 def load_housing_data(housing_path=HOUSING_PATH):
-    csv_path = os.path.join("./", "housing.csv")
+    csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
 
+
 housing = load_housing_data()
-housing.head(10)
+print(housing.head(10))
+
+print(housing.info())
+
+
+print(housing["ocean_proximity"].value_counts())
+
+
+print(housing.describe())
+
+
+%matplotlib inline
+import matplotlib.pyplot as plt
+housing.hist(bins=50, figsize=(20,15))
+plt.show()
