3a5200705c60c34d2f16d60e6810c4f09deac52e
Jonghun Shin <69336570+ozingmw@users.noreply.github.com>
2022-01-02 08:37:51 +0900

    22.01.02

---
+++ file_download.py
@@ -0,0 +1,14 @@
+import os
+import tarfile
+import urllib.request
+class github:
+    def download(filename, url, path):
+        if filename is None:
+            filename = path.split("/")[-1]
+        if os.path.isfile(os.path.join(path, f'{filename}.csv')): return
+        os.makedirs(path, exist_ok=True)
+        tgz_path = os.path.join(path, f'{filename}.tgz')
+        urllib.request.urlretrieve(url, tgz_path)
+        housing_tgz = tarfile.open(tgz_path)
+        housing_tgz.extractall(path=path)
+        housing_tgz.close()
---
+++ file_load.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+class csv:
+    def load(path): return pd.read_csv(path)
