3ec00044971f3223e44df707a41be7eb5df8163d
Jonghun Shin <69336570+ozingmw@users.noreply.github.com>
2022-01-03 17:57:39 +0900

    22.01.02

--- califonia_house_price.py
+++ califonia_house_price.py
@@ -69,3 +69,4 @@ split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
 for train_index, test_index in split.split(housing, housing["income_cat"]):
     strat_train_set = housing.loc[train_index]
     strat_test_set = housing.loc[test_index]
+print(strat_test_set["income_cat"].value_counts() / len(strat_test_set))
