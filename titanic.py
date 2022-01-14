a98555baeb6036112ee9e236a10b70603221148f
Jonghun Shin <ozingmw@naver.com>
2022-01-14 23:12:03 +0900

    22.01.15

--- titanic.py
+++ titanic.py
@@ -1,12 +1,16 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from sklearn.impute import KNNImputer
+from sklearn.impute import KNNImputer, SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer, make_column_selector
 
 train_data = pd.read_csv("./datasets/titanic/train.csv")
 test_data = pd.read_csv("./datasets/titanic/test.csv")
 
 print(train_data.info())
+print(train_data.describe())
 
 train_data = train_data.set_index("PassengerId")
 test_data = test_data.set_index("PassengerId")
@@ -18,12 +22,21 @@ print(train_data.head(10))
 
 print(train_data["Sex"].value_counts())
 
-imputer_1 = KNNImputer(weights="distance")
-age_by_class_1 = train_data.loc[train_data["Pclass"] == 1]["Age"]
-train_data["Age"] = imputer_1.fit_transform(age_by_class_1)
-print(train_data["Age"].isnull())
-
 num_pipeline = Pipeline([
-        ("imputer", SimpleImputer(strategy="median")),
-        ("scaler", StandardScaler())
-    ])
+    ("imputer", KNNImputer()),
+    ("scaler", StandardScaler())
+])
+
+cat_pipeline = Pipeline([
+    ("imputer", SimpleImputer(strategy="most_frequent")),
+    ("scaler", OneHotEncoder())
+])
+
+full_pipeline = ColumnTransformer([
+    ("num", num_pipeline, make_column_selector(dtype_exclude=object)),
+    ("cat", cat_pipeline, make_column_selector(dtype_include=object))
+])
+
+train_data_fp = full_pipeline.fit_transform(train_data)
+
+print(train_data_fp.info())
