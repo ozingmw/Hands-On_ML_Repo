d8cedc35d8d59ce0a32e9a8da1b18b50e29c3d8e
Jonghun Shin <ozingmw@naver.com>
2022-01-18 20:49:23 +0900

    22.01.19

---
+++ spam_assassin.py
@@ -0,0 +1,58 @@
+import os
+import email
+import email.policy
+import email.parser
+
+from collections import Counter
+
+SPAM_PATH = os.path.join("datasets", "spam")
+HAM_DIR = os.path.join(SPAM_PATH, "easy_ham")
+SPAM_DIR = os.path.join(SPAM_PATH, "spam")
+ham_filenames = [name for name in sorted(os.listdir(HAM_DIR)) if len(name) > 20]
+spam_filenames = [name for name in sorted(os.listdir(SPAM_DIR)) if len(name) > 20]
+
+def load_email(is_spam, filename, spam_path=SPAM_PATH):
+    directory = "spam" if is_spam else "easy_ham"
+    with open(os.path.join(spam_path, directory, filename), "rb") as f:
+        return email.parser.BytesParser(policy=email.policy.default).parse(f)
+    
+ham_emails = [load_email(is_spam=False, filename=name) for name in ham_filenames]
+spam_emails = [load_email(is_spam=True, filename=name) for name in spam_filenames]
+
+# print(ham_emails[0].get_content().strip())
+# print(spam_emails[1].get_content().strip())
+
+def get_email_structure(email):
+    if isinstance(email, str):
+        return email
+    payload = email.get_payload()
+    if isinstance(payload, list):
+        return "multipart({})".format(", ".join([
+            get_email_structure(sub_email)
+            for sub_email in payload
+        ]))
+    else:
+        return email.get_content_type()
+    
+def structures_counter(emails):
+    structures = Counter()
+    for email in emails:
+        structure = get_email_structure(email)
+        structures[structure] += 1
+    return structures
+
+print(structures_counter(ham_emails).most_common())
+print(structures_counter(spam_emails).most_common())
+
+for header, value in spam_emails[0].items():
+    print(header,":",value)
+
+print(spam_emails[0]["Subject"])
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+X = np.array(ham_emails + spam_emails, dtype=object)
+y = np.array([0] * len(ham_emails) + [1] * len(spam_emails))
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
