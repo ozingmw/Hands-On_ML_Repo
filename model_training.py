f5cbf51f71da580ffd28d75e3e2fa4ff2e0f54b0
Jonghun Shin <ozingmw@naver.com>
2022-02-03 20:47:40 +0900

    22.02.03

--- model_training.py
+++ model_training.py
@@ -74,3 +74,51 @@ X_poly = poly_features.fit_transform(X)
 lin_reg = LinearRegression()
 lin_reg.fit(X_poly, y)
 print(lin_reg.intercept_, lin_reg.coef_)
+
+
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+def plot_learning_curves(model, X, y):
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
+    train_errors, val_errors = [], []
+    for m in range(1, len(X_train)):
+        model.fit(X_train[:m], y_train[:m])
+        y_train_predict = model.predict(X_train[:m])
+        y_val_predict = model.predict(X_val)
+        train_errors.append(mean_squared_error(y_train[:m], y_train_predict))
+        val_errors.append(mean_squared_error(y_val, y_val_predict))
+    
+    plt.plot(np.sqrt(train_errors), "r-+", linewidth=2, label="training set")
+    plt.plot(np.sqrt(val_errors), "b-", linewidth=3, label="validation set")
+
+lin_reg = LinearRegression()
+plot_learning_curves(lin_reg, X, y)
+
+
+from sklearn.pipeline import Pipeline
+ploynomial_regression = Pipeline([
+    ("poly_features", PolynomialFeatures(degree=10, include_bias=False)),
+    ("lin_reg", LinearRegression()),
+])
+plot_learning_curves(ploynomial_regression, X, y)
+# plt.axis([0, 80, 0, 3])
+# plt.show() 
+
+from sklearn.linear_model import Ridge, Lasso, ElasticNet
+ridge_reg = Ridge(alpha=1, solver="cholesky")
+ridge_reg.fit(X, y)
+print(ridge_reg.predict([[1.5]]))
+
+sgd_reg = SGDRegressor(penalty="l2")
+sgd_reg.fit(X, y.ravel())
+print(sgd_reg.predict([[1.5]]))
+
+lasso_reg = Lasso(alpha=0.1)
+lasso_reg.fit(X, y)
+print(lasso_reg.predict([[1.5]]))
+
+elastic_net = ElasticNet(alpha=0.1, l1_ratio=0.5)
+elastic_net.fit(X, y)
+print(elastic_net.predict([[1.5]]))
