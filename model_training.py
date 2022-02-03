1f4d0953f334d3c5fdc22c67a00e041aec8ada21
Jonghun Shin <ozingmw@naver.com>
2022-02-03 16:34:41 +0900

    22.02.03

---
+++ model_training.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+X = 2 * np.random.randn(100, 1)
+y = 4 + 3 * X + np.random.randn(100, 1)
+
+X_b = np.c_[np.ones((100, 1)), X]
+theta_best = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y)
+print(theta_best)
+
+X_new = np.array([[0], [2]])
+X_new_b = np.c_[np.ones((2,1)), X_new]
+y_predict = X_new_b.dot(theta_best)
+print(y_predict)
+
+
+from sklearn.linear_model import LinearRegression
+
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+print(lin_reg.intercept_, lin_reg.coef_)
+print(lin_reg.predict(X_new))
+
+
+theta_best_svd, residuals, rank, s = np.linalg.lstsq(X_b, y, rcond=1e-6)
+print(theta_best_svd)
+print(np.linalg.pinv(X_b).dot(y))
+
+
+eta = 0.1
+n_iterations = 1000
+m = 100
+
+theta = np.random.randn(2, 1)
+
+for iteration in range(n_iterations):
+    gradients = 2/m * X_b.T.dot(X_b.dot(theta)-y)
+    theta = theta - eta*gradients
+print(theta)
+
+
+n_epochs = 50
+t0, t1 = 5, 50
+
+def learning_schedule(t):
+    return t0 / (t + t1)
+
+theta = np.random.randn(2,1)
+
+for epoch in range(n_epochs):
+    for i in range(m):
+        random_index = np.random.randint(m)
+        xi = X_b[random_index:random_index+1]
+        yi = y[random_index:random_index+1]
+        gradients = 2 * xi.T.dot(xi.dot(theta) - yi)
+        eta = learning_schedule(epoch * m + i)
+        theta = theta - eta * gradients
+print(theta)
+
+
+from sklearn.linear_model import SGDRegressor
+sgd_reg = SGDRegressor(max_iter=1000, tol=1e-3, penalty=None, eta0=0.1)
+sgd_reg.fit(X, y.ravel())
+print(sgd_reg.intercept_, sgd_reg.coef_)
+
+
+m = 100
+X = 6 * np.random.rand(m, 1) - 3
+y = 0.5 * X**2 + X + 2 + np.random.randn(m, 1)
+
+from sklearn.preprocessing import PolynomialFeatures
+poly_features = PolynomialFeatures(degree=2, include_bias=False)
+X_poly = poly_features.fit_transform(X)
+
+lin_reg = LinearRegression()
+lin_reg.fit(X_poly, y)
+print(lin_reg.intercept_, lin_reg.coef_)
