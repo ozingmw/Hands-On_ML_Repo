254eb14aed52b8dc2d750293a5a2553a4e55fedb
Jonghun Shin <ozingmw@naver.com>
2022-02-04 16:59:50 +0900

    pratice 12

--- iris_classification.py
+++ iris_classification.py
@@ -27,3 +27,88 @@ softmax_reg.fit(X, y)
 
 print(softmax_reg.predict_proba([[5, 2]]))
 print(softmax_reg.predict([[5, 2]]))
+
+
+
+X = iris["data"][:, (2,3)]
+y = iris["target"]
+X_with_bias = np.c_[np.ones([len(X), 1]), X]
+
+test_ratio = 0.2
+validation_ratio = 0.2
+
+total_size = len(X_with_bias)
+test_size = int(total_size * test_ratio)
+validation_size = int(total_size * validation_ratio)
+train_size = total_size - test_size - validation_size
+
+rnd_indices = np.random.permutation(total_size)
+X_train = X_with_bias[rnd_indices[:train_size]]
+y_train = y[rnd_indices[:train_size]]
+X_valid = X_with_bias[rnd_indices[train_size:-test_size]]
+y_valid = y[rnd_indices[train_size:-test_size]]
+X_test = X_with_bias[rnd_indices[-test_size:]]
+y_test = y[rnd_indices[-test_size:]]
+
+def to_one_hot(y):
+    n_classes = y.max() + 1
+    m = len(y)
+    Y_one_hot = np.zeros((m, n_classes))
+    Y_one_hot[np.arange(m), y] = 1
+    return Y_one_hot
+
+Y_train_one_hot = to_one_hot(y_train)
+Y_valid_one_hot = to_one_hot(y_valid)
+Y_test_one_hot = to_one_hot(y_test)
+
+def softmax(logits):
+    exps = np.exp(logits)
+    exp_sums = np.sum(exps, axis=1, keepdims=True)
+    return exps / exp_sums
+
+n_inputs = X_train.shape[1]
+n_outputs = len(np.unique(y_train))
+
+eta = 0.1 
+n_iterations = 5001
+m = len(X_train)
+epsilon = 1e-7
+alpha = 0.1  
+best_loss = np.infty
+
+Theta = np.random.randn(n_inputs, n_outputs)
+
+for iteration in range(n_iterations):
+    logits = X_train.dot(Theta)
+    Y_proba = softmax(logits)
+    error = Y_proba - Y_train_one_hot
+    gradients = 1/m * X_train.T.dot(error) + np.r_[np.zeros([1, n_outputs]), alpha * Theta[1:]]
+    Theta = Theta - eta * gradients
+
+    logits = X_valid.dot(Theta)
+    Y_proba = softmax(logits)
+    xentropy_loss = -np.mean(np.sum(Y_valid_one_hot * np.log(Y_proba + epsilon), axis=1))
+    l2_loss = 1/2 * np.sum(np.square(Theta[1:]))
+    loss = xentropy_loss + alpha * l2_loss
+    if iteration % 500 == 0:
+        print(iteration, loss)
+    if loss < best_loss:
+        best_loss = loss
+    else:
+        print(iteration - 1, best_loss)
+        print(iteration, loss, "조기 종료!")
+        break
+        
+logits = X_valid.dot(Theta)
+Y_proba = softmax(logits)
+y_predict = np.argmax(Y_proba, axis=1)
+
+accuracy_score = np.mean(y_predict == y_valid)
+print(accuracy_score)
+
+logits = X_test.dot(Theta)
+Y_proba = softmax(logits)
+y_predict = np.argmax(Y_proba, axis=1)
+
+accuracy_score = np.mean(y_predict == y_test)
+print(accuracy_score)
