324d43c70b1e013e9e554884ad00c9e50bcddcd7
Jonghun Shin <ozingmw@naver.com>
2022-02-08 21:23:02 +0900

    22.02.08

--- mnist_ensemble_forest.py
+++ mnist_ensemble_forest.py
@@ -1,35 +1,25 @@
 from sklearn.datasets import fetch_openml
-from sklearn.ensemble import RandomForestClassifier, VotingClassifier
-from sklearn.linear_model import LogisticRegression, SGDClassifier, LinearRegression
-from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, VotingClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
-from sklearn.tree import ExtraTreeClassifier
+from sklearn.svm import LinearSVC
+from sklearn.neural_network import MLPClassifier
 
 mnist = fetch_openml('mnist_784', version=1, cache=True)
 X_train, X_test, y_train, y_test = train_test_split(mnist.data, mnist.target, test_size=10000)
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=10000)
 
 rnd_clf = RandomForestClassifier(random_state=42)
-extree_clf = ExtraTreeClassifier(random_state=42)
-svm_clf = SVC(random_state=42)
-svm_clf_per = SVC(probability=True, random_state=42)
-sgd_clf = SGDClassifier(random_state=42)
+extree_clf = ExtraTreesClassifier(random_state=42)
+svm_clf = LinearSVC(random_state=42)
+mlp_clf = MLPClassifier(random_state=42)
 
-vote_clf = VotingClassifier(
-    estimators=[("rnd", rnd_clf), ("extree", extree_clf), ("svm", svm_clf), ("sgd", sgd_clf)],
-    voting="hard",
-)
-
-for clf in (rnd_clf, extree_clf, svm_clf, sgd_clf, vote_clf):
-    clf.fit(X_train, y_train)
-    print(clf.__class__.__name__, clf.score(X_val, y_val))
+estimators = [rnd_clf, extree_clf, svm_clf, mlp_clf]
+for estimator in estimators:
+    estimator.fit(X_train, y_train)
     
+print([estimator.score(X_val, y_val) for estimator in estimators])
 
-vote_clf = VotingClassifier(
-    estimators=[("rnd", rnd_clf), ("extree", extree_clf), ("svm", svm_clf_per), ("sgd", sgd_clf)],
-    voting="soft",
-)
-
-vote_clf.fit(X_train, y_train)
-print(vote_clf.score(X_val, y_val))
+voting_clf = VotingClassifier(estimators=[("rnd", rnd_clf), ("extree", extree_clf), ("svm", svm_clf), ("mlp", mlp_clf)])
+voting_clf.fit(X_train, y_train)
+print(voting_clf.score(X_val, y_val))
+print([estimator.score(X_val, y_val) for estimator in voting_clf.estimators_])
