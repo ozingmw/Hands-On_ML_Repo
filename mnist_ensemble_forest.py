10c5dce121ca43df02fc7fe4c49c55603830af70
Jonghun Shin <ozingmw@naver.com>
2022-02-09 15:29:18 +0900

    22.02.09

--- mnist_ensemble_forest.py
+++ mnist_ensemble_forest.py
@@ -1,3 +1,4 @@
+import numpy as np
 from sklearn.datasets import fetch_openml
 from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, VotingClassifier
 from sklearn.model_selection import train_test_split
@@ -5,12 +6,12 @@ from sklearn.svm import LinearSVC
 from sklearn.neural_network import MLPClassifier
 
 mnist = fetch_openml('mnist_784', version=1, cache=True)
-X_train, X_test, y_train, y_test = train_test_split(mnist.data, mnist.target, test_size=10000)
+X_train, X_test, y_train, y_test = train_test_split(mnist.data, mnist.target.astype(np.uint8), test_size=10000)
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=10000)
 
 rnd_clf = RandomForestClassifier(random_state=42)
 extree_clf = ExtraTreesClassifier(random_state=42)
-svm_clf = LinearSVC(random_state=42)
+svm_clf = LinearSVC(max_iter=100, tol=20 , random_state=42)
 mlp_clf = MLPClassifier(random_state=42)
 
 estimators = [rnd_clf, extree_clf, svm_clf, mlp_clf]
@@ -23,3 +24,14 @@ voting_clf = VotingClassifier(estimators=[("rnd", rnd_clf), ("extree", extree_cl
 voting_clf.fit(X_train, y_train)
 print(voting_clf.score(X_val, y_val))
 print([estimator.score(X_val, y_val) for estimator in voting_clf.estimators_])
+
+del voting_clf.estimators_[2]
+print(voting_clf.score(X_val, y_val))
+
+voting_clf.voting = "soft"
+print(voting_clf.score(X_val, y_val))
+
+voting_clf.voting = "hard"
+print(voting_clf.score(X_test, y_test))
+print([estimator.score(X_test,y_test) for estimator in voting_clf.estimators_])
+
