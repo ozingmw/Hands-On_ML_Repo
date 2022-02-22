0fad90afae90789852e3beeb2ffe3b724f728d96
Jonghun Shin <ozingmw@naver.com>
2022-02-22 20:49:35 +0900

    22.02.22

--- titanic_pipeline.py
+++ titanic_pipeline.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.impute import KNNImputer
 
@@ -6,7 +8,7 @@ class FamilyAddAttrib(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
         return self
     def transform(self, X):
-        X["Family"] = X["SibSp"] + X["Parch"]
+        return np.c_[X, X["SibSp"] + X["Parch"]]
 
 class AgeTransform(BaseEstimator, TransformerMixin):
     def __init__(self):
@@ -26,4 +28,3 @@ class CabinTransform(BaseEstimator, TransformerMixin):
         X["Cabin"] = X["Cabin"].str[:1]
         X["Cabin"].fillna("Na", inplace=True)
         return X
-2
